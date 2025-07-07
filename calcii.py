8714b1ab9834943e430a63daccafbea4014e5b4c
Vishnu8767 <boddu.reddy@s.amity.edu>
2025-07-07 14:33:15 +0530

    added sum and diff of two numbers

--- calcii.py
+++ calcii.py
@@ -5,4 +5,15 @@ print("o = division")
 
 m = input("Enter your operator: ")
 
+if m == "a":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z + j
+    print(y, "is the sum")
+
+elif m == "b":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z - j
+    print(y, "is the difference")
 
