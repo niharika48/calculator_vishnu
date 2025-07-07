a95d19add0469036487c212f9888fb934ead7cfb
Vishnu8767 <boddu.reddy@s.amity.edu>
2025-07-07 14:38:01 +0530

    ARTHMATIC CALCULATOR IS DONE

--- calcii.py
+++ calcii.py
@@ -21,4 +21,14 @@ elif m == "c":
     j = int(input("Enter num 2: "))
     y = z * j
     print(y, "is the product")
+elif m == "o":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    if j != 0:
+        y = z / j
+        print(y, "is the division")
+    else:
+        print("Error: Division by zero")
 
+else:
+    print("Error: Invalid operator")
