d03e2f00adc8308ef1a4aba9eb81280894ef9c88
Vishnu8767 <boddu.reddy@s.amity.edu>
2025-07-07 14:35:31 +0530

    added product of two num

--- calcii.py
+++ calcii.py
@@ -16,4 +16,9 @@ elif m == "b":
     j = int(input("Enter num 2: "))
     y = z - j
     print(y, "is the difference")
+elif m == "c":
+    z = int(input("Enter num 1: "))
+    j = int(input("Enter num 2: "))
+    y = z * j
+    print(y, "is the product")
 
