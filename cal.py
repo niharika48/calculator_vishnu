00926011654c55871ef9e55ae34101c237ff3d7f
niharika48 <niharikasiri2006@gmail.com>
2025-07-28 12:02:45 +0530

    creted a new file

---
+++ cal.py
@@ -0,0 +1,9 @@
+a=int(input("enter a number"))
+b=int(input("enter a second number"))
+operator=int(input("enter a operator"))
+if(operator='sum')
+ print("the sum",a+b)
+elif(operator='sub')
+print("the sub",a-b)
+else
+print("invalid ")
